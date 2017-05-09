68e23187622adbd82305ce3c4894bef07e17f65a
Dedi <Dedi@DESKTOP-9O8E26U>
2017-05-09 14:52:39 +0700

    first

---
+++ src/LinkedList/SingleList.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+public class SingleList {
+	Node head, tail;
+	int size;
+	
+	public void inisialisasi() {
+		head=tail=null;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void addFirst(Object o) {
+		Node input = new Node();
+		input.data = o;
+		if(isEmpty()) {
+			head=input;
+			tail=input;
+		}
+		else {
+			input.next=head;
+			head=input;
+		}
+		size++;
+	}
+	
+	public void addLast(Object o) {
+		Node input = new Node();
+		input.data= o ;
+		if (isEmpty()) {
+			head=input;
+			tail=input;
+		}
+		else {
+			tail.next=input;
+			tail=tail.next;
+		}
+		size++;
+	}
+	
+	public void print() {
+		Node temp = head;
+		if(!isEmpty()) {
+			while(temp != null) {
+				System.out.println(temp.data);
+				temp = temp.next;
+			}
+		}
+		else {
+			System.out.println("Data kosong");
+		}
+	}
+}
